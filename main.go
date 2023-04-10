bfede8d4b51ac91b0e23b64ca1cbba9038789a9c
Divyansh Singh <sdivyansh59@gmail.com>
2023-04-10 18:45:32 +0530

    Ep-1 w.WriteHeader() && w.Write() === http.Error()

--- main.go
+++ main.go
@@ -11,13 +11,22 @@ func main() {
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
 		log.Println("Hello World")
-		d, _ := ioutil.ReadAll(r.Body)
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Oops"))
+			
+			// line 19 is === 16 & 17
+			http.Error(w,"Oops", http.StatusBadRequest)
+			return 
+		}
 
 		log.Printf("Data %s ", d) // convert []bytes in string and print
 		log.Println(d) // print []byte (which is d)
 
 		// write back to user using responseWriter
 		fmt.Fprintf(w, "hello %s ","Divyansh")
+
 		
 	})
 
